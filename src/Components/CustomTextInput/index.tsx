753a3df00dd167adcc4d706f18402d998f5035aa
Orhan Kahraman <herorhan999@gmail.com>
2024-05-23 11:42:51 +0300

    Added: Filters and search structure added to tables

--- src/Components/CustomTextInput/index.tsx
+++ src/Components/CustomTextInput/index.tsx
@@ -6,61 +6,58 @@ import { twMerge } from "tailwind-merge";
 
 interface CustomTextInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  id: string;
   label?: string;
-  value: string;
-  placeholder: string;
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   isError?: boolean;
   wrapperClassName?: string;
+  isOpenClearTextButton?: boolean;
 }
 
 const CustomTextInput: React.FC<CustomTextInputProps> = ({
-  id,
   label,
-  value,
-  onChange,
-  placeholder,
   isError = false,
   className,
   wrapperClassName,
+  isOpenClearTextButton = true,
   ...props
 }) => {
   const clearInput = () => {
-    // Trigger the onChange function by creating a dummy event
-    const event = {
-      target: { value: "" },
-      currentTarget: { value: "" },
-    } as unknown as ChangeEvent<HTMLInputElement>;
-    onChange(event);
+    if (props.onChange) {
+      // Trigger the onChange function by creating a dummy event
+      const event = {
+        target: { value: "" },
+        currentTarget: { value: "" },
+      } as unknown as ChangeEvent<HTMLInputElement>;
+      props.onChange(event);
+    }
   };
 
   return (
     <div id="custom-text-input" className={wrapperClassName}>
-      <div className="flex flex-row justify-between mb-2">
-        <CustomLabel title={label} htmlFor={id} />
-        {props.maxLength && (
-          <div className="text-right text-xs font-mono mt-1 mr-2 cursor-default">
-            {value.length}/{props.maxLength}
-          </div>
-        )}
-      </div>
+      {label && (
+        <div className="flex flex-row justify-between mb-2">
+          <CustomLabel title={label} htmlFor={props.id} />
+          {props.maxLength && (
+            <div className="text-right text-xs font-mono mt-1 mr-2 cursor-default">
+              {props.value ? (props.value as string).length : 0}/
+              {props.maxLength}{" "}
+            </div>
+          )}
+        </div>
+      )}
       <div className="relative">
         <input
-          id={id}
-          placeholder={placeholder}
           type="text"
-          value={value}
-          onChange={onChange}
           className={twMerge(
             `${
               isError ? "border-error" : ""
-            } border-2 bg-transparent border-background/50 p-2 rounded-lg w-full focus:border-primary focus:ring-0 text-text-darkest dark:text-text-lightest transition-colors duration-300`,
+            } border-2 bg-transparent border-background/50 p-2 ${
+              props.value && isOpenClearTextButton ? "pr-10 " : ""
+            } rounded-lg w-full focus:border-primary focus:ring-0 text-text-darkest dark:text-text-lightest transition-colors duration-300`,
             className
           )}
           {...props}
         />
-        {value && (
+        {props.value && isOpenClearTextButton && (
           <MdCancel
             size={22}
             onClick={clearInput}
--- src/Components/GenericTable/index.tsx
+++ src/Components/GenericTable/index.tsx
@@ -1,6 +1,6 @@
 // src/Components/GenericTable/index.tsx
-import React, { useMemo, useState } from "react";
-import { IoIosMore } from "react-icons/io";
+import React, { useCallback, useMemo, useState } from "react";
+import { IoIosCloseCircle, IoIosMore } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import DropdownMenu from "../DropdownMenu";
 import { FaCircleArrowDown, FaCircleArrowUp } from "react-icons/fa6";
@@ -11,6 +11,8 @@ import { getInitialSortConfig } from "../../Utils/getInitialSortConfig";
 import Skeleton from "react-loading-skeleton";
 import AsyncIconButton from "../Buttons/AsyncIconButton";
 import { IoMdMore } from "react-icons/io";
+import CustomTextInput from "../CustomTextInput";
+import { debounce } from "lodash";
 
 const TableHeaderCell: React.FC<{
   children: React.ReactNode;
@@ -22,7 +24,7 @@ const TableHeaderCell: React.FC<{
     onClick={onClick}
     className={` ${
       onClick ? "cursor-pointer" : "cursor-default"
-    } relative px-5 py-3 text-xs text-center font-semibold uppercase tracking-wider border-r last:border-r-0 border-text-lighter dark:border-text-dark `}
+    } relative px-2 py-3 text-xs text-center font-semibold uppercase tracking-wider border-r last:border-r-0 border-text-lighter dark:border-text-dark `}
   >
     {children}
     {isSorted && (
@@ -33,8 +35,8 @@ const TableHeaderCell: React.FC<{
   </th>
 );
 
-const TableCell: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <td className="px-5 py-3 text-sm text-center cursor-default border-r last:border-r-0 border-text-lighter dark:border-text-dark">
+const TableCell: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
+  <td className="px-2 py-3 text-sm text-center cursor-default border-r last:border-r-0 border-text-lighter dark:border-text-dark">
     {children}
   </td>
 );
@@ -49,6 +51,13 @@ const GenericTable = <T extends {}>(props: TableProps<T>) => {
     getTableActions,
   } = props;
   const [activeDropdown, setActiveDropdown] = useState<number | null>(null);
+  const [searchTerms, setSearchTerms] = useState<Record<keyof T, string>>(
+    {} as Record<keyof T, string>
+  );
+  const [liveSearchTerms, setLiveSearchTerms] = useState<
+    Record<keyof T, string>
+  >({} as Record<keyof T, string>);
+
   const [isActiveTableDropdown, setIsActiveTableDropdown] =
     useState<boolean>(false);
 
@@ -67,6 +76,46 @@ const GenericTable = <T extends {}>(props: TableProps<T>) => {
       data.some((item) => dropdownOptions && dropdownOptions(item).length > 0),
     [data, dropdownOptions]
   );
+
+  const handleSearchChange = useCallback(
+    debounce((key: keyof T, value: string) => {
+      setSearchTerms((prev) => ({ ...prev, [key]: value }));
+    }, 300),
+    []
+  );
+
+  // Instant update function for input fields
+  const handleInputChange = (key: keyof T, value: string) => {
+    setLiveSearchTerms((prev) => ({ ...prev, [key]: value }));
+    handleSearchChange(key, value);
+  };
+
+  // Calculate filtered data
+  const filteredData = useMemo(() => {
+    return sortedItems.filter((item) => {
+      return Object.keys(searchTerms).every((key) => {
+        const searchTerm = searchTerms[key as keyof T].toLowerCase();
+        const column = columns.find((col) => col.key === key);
+        let itemValue = "";
+
+        if (column?.render) {
+          const renderedContent = column.render(item);
+          if (typeof renderedContent === "string") {
+            itemValue = renderedContent.toLowerCase();
+          } else {
+            return true;
+          }
+        } else {
+          const renderedContent = renderContent(item[key as keyof T]);
+          if (typeof renderedContent === "string") {
+            itemValue = renderedContent.toLowerCase();
+          }
+        }
+
+        return itemValue.includes(searchTerm);
+      });
+    });
+  }, [sortedItems, searchTerms, columns]);
 
   return (
     <div>
@@ -94,7 +143,11 @@ const GenericTable = <T extends {}>(props: TableProps<T>) => {
       ) : data.length > 0 ? (
         <div>
           <div className="flex flex-row justify-between w-full mb-2">
-            <div className="font-light">{data.length} veri listeleniyor</div>
+            <div className="font-light">
+              {data.length} veriden <strong>{filteredData.length}</strong> veri
+              listeleniyor
+            </div>
+
             {getTableActions && (
               <div className="relative">
                 <AsyncIconButton
@@ -134,57 +187,99 @@ const GenericTable = <T extends {}>(props: TableProps<T>) => {
                   </TableHeaderCell>
                 ))}
                 {showDropdownColumn && (
-                  <TableHeaderCell>İşlemler</TableHeaderCell>
+                  <TableHeaderCell>
+                    <IoIosMore size={22} />
+                  </TableHeaderCell>
+                )}
+              </tr>
+              <tr className="border-b border-text-lighter dark:border-text-dark">
+                {columns.map((column, columnIndex) => (
+                  <TableCell key={`search-${columnIndex}`}>
+                    <CustomTextInput
+                      id={`search-${columnIndex}`}
+                      placeholder={`Ara...`}
+                      isOpenClearTextButton={false}
+                      value={liveSearchTerms[column.key as keyof T] || ""}
+                      onChange={(e) =>
+                        handleInputChange(column.key, e.target.value)
+                      }
+                    />
+                  </TableCell>
+                ))}
+                {showDropdownColumn && (
+                  <TableCell>
+                    <button
+                      className="hover:text-primary"
+                      onClick={() => {
+                        setSearchTerms({} as Record<keyof T, string>);
+                        setLiveSearchTerms({} as Record<keyof T, string>);
+                      }}
+                    >
+                      <IoIosCloseCircle size={22} />
+                    </button>
+                  </TableCell>
                 )}
               </tr>
             </thead>
             <tbody>
-              {sortedItems.map((item, rowIndex) => (
-                <tr
-                  key={rowIndex}
-                  className={`${
-                    rowIndex % 2 === 0
-                      ? "bg-transparent"
-                      : "bg-text-lighter bg-opacity-30"
-                  } text-text-darkest dark:text-text-lightest transition-colors duration-300 ease-in-out`}
-                >
-                  {columns.map((column, colIndex) => (
-                    <TableCell key={`${rowIndex}-${colIndex}`}>
-                      {column.render
-                        ? column.render(item)
-                        : renderContent(item[column.key as keyof T])}
-                    </TableCell>
-                  ))}
-                  {showDropdownColumn && (
-                    <TableCell>
-                      <div className="relative inline-block">
-                        <button
-                          className="hover:text-primary"
-                          onClick={(event) => {
-                            event.stopPropagation();
-                            setActiveDropdown(
-                              activeDropdown === rowIndex ? null : rowIndex
-                            );
-                          }}
-                        >
-                          {activeDropdown === rowIndex ? (
-                            <IoClose size={20} className="text-error" />
-                          ) : (
-                            <IoIosMore size={20} />
+              {filteredData.length > 0 ? (
+                filteredData.map((item, rowIndex) => (
+                  <tr
+                    key={rowIndex}
+                    className={`${
+                      rowIndex % 2 === 0
+                        ? "bg-transparent"
+                        : "bg-text-lighter bg-opacity-30"
+                    } text-text-darkest dark:text-text-lightest transition-colors duration-300 ease-in-out`}
+                  >
+                    {columns.map((column, colIndex) => (
+                      <TableCell key={`${rowIndex}-${colIndex}`}>
+                        {column.render
+                          ? column.render(item)
+                          : renderContent(item[column.key as keyof T])}
+                      </TableCell>
+                    ))}
+                    {showDropdownColumn && (
+                      <TableCell>
+                        <div className="relative inline-block">
+                          <button
+                            className="hover:text-primary"
+                            onClick={(event) => {
+                              event.stopPropagation();
+                              setActiveDropdown(
+                                activeDropdown === rowIndex ? null : rowIndex
+                              );
+                            }}
+                          >
+                            {activeDropdown === rowIndex ? (
+                              <IoClose size={22} className="text-error" />
+                            ) : (
+                              <IoIosMore size={22} />
+                            )}
+                          </button>
+                          {activeDropdown === rowIndex && (
+                            <DropdownMenu
+                              options={dropdownOptions!(item)}
+                              closeDropdown={() => setActiveDropdown(null)}
+                              id={rowIndex}
+                            />
                           )}
-                        </button>
-                        {activeDropdown === rowIndex && (
-                          <DropdownMenu
-                            options={dropdownOptions!(item)}
-                            closeDropdown={() => setActiveDropdown(null)}
-                            id={rowIndex}
-                          />
-                        )}
-                      </div>
-                    </TableCell>
-                  )}
+                        </div>
+                      </TableCell>
+                    )}
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={columns.length + (showDropdownColumn ? 1 : 0)}>
+                    <div className="text-center py-5">
+                      <span className="text-xl opacity-50 cursor-default">
+                        Aradığınız kriterlere göre veri bulunamadı
+                      </span>
+                    </div>
+                  </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
